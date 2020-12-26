510a84038ca0719c19d2b19887452333ff329137
Alexey Fadeev <afadeev@iMac-Alexey.local>
2020-12-26 20:07:47 +0300

    ver 1.0

--- fill_max_slow_dag.py
+++ fill_max_slow_dag.py
@@ -2,6 +2,14 @@
 Даг берет записи из таблицы public.noise_tracks, у которых max_slow_time is null and max_slow = 0.
 Затем из полученной таблицы для каждой записи делает запрос к сырым данным eco_monitoring.noise_raw_data и
 согласно first_time_data & last_time_data находит max_slow & max_slow_time
+
+Для работы DockerOperator необходимо:
+1) в Dockerfile прописать установку:
+    RUN pip install docker==4.1.0
+2) в docker-compouse.yml
+    volumes:
+        # Mount the docker socket from the host (currently my laptop) into the webserver container
+        - //var/run/docker.sock:/var/run/docker.sock
 """
 
 import os
